c504c714ef7b7ae225383670c9fc9d911f8ea978
Taimisson C. <taimisson2003@hotmail.com>
2024-05-14 10:56:16 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,30 @@
+package boardgame;
+
+public class Board {
+
+        private int rows;
+        private int columns;
+        private Piece[][] pieces;
+
+        public Board(int columns, int rows) {
+            this.columns = columns;
+            this.rows = rows;
+            pieces = new Piece[columns][rows];
+        }
+
+        public int getRows() {
+            return rows;
+        }
+
+        public void setRows(int rows) {
+            this.rows = rows;
+        }
+
+        public int getColumns() {
+            return columns;
+        }
+
+        public void setColumns(int columns) {
+            this.columns = columns;
+        }
+}
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,32 +2,32 @@ package boardgame;
 
 public class Position {
 
-    private int row;
-    private int column;
+        private int row;
+        private int column;
 
-    public Position(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
+        public Position(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
 
-    public int getRow() {
-        return row;
-    }
+        public int getRow() {
+            return row;
+        }
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+        public void setRow(int row) {
+            this.row = row;
+        }
 
-    public int getColumn() {
-        return column;
-    }
+        public int getColumn() {
+            return column;
+        }
 
-    public void setColumn(int column) {
-        this.column = column;
-    }
+        public void setColumn(int column) {
+            this.column = column;
+        }
 
-    @Override
-    public String toString() {
-        return row + ", " + column;
-    }
+        @Override
+        public String toString() {
+            return row + ", " + column;
+        }
 }
