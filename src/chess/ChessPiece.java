6445b50817637b9e390e4ef6b6ac684ba963c4a4
Taimisson C. <taimisson2003@hotmail.com>
2024-05-14 18:40:53 -0300

    Implementing possible moves of Rook

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,8 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
+
 
 public abstract class ChessPiece extends Piece {
 
@@ -12,9 +14,16 @@ public abstract class ChessPiece extends Piece {
             this.color = color;
         }
 
-    public Color getColor() {
-        return color;
-    }
+        public Color getColor() {
+            return color;
+        }
+
+        protected boolean isThereOpponentPiece(Position position){
+            ChessPiece p = (ChessPiece) getBoard().piece(position);
+            return p != null && p.getColor() != color;
+        }
+
+        
 
 
 }
