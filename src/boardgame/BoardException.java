9dab0430da1d2da6a3af31619d13af9a524e4c73
Taimisson C. <taimisson2003@hotmail.com>
2024-05-14 13:16:26 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+import java.io.Serial;
+
+public class BoardException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+}
